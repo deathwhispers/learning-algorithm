a98709031428113c43a8c2da6df62555c97bb6cf
杨光剑 <yangguangjian@rhytech.com.cn>
2022-10-12 16:42:51 +0800

    numComponents

---
+++ src/main/java/com/guangjian/algorithm/daily/AreAlmostEqual.java
@@ -0,0 +1,83 @@
+package com.guangjian.algorithm.daily;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 1790. 仅执行一次字符串交换能否使两个字符串相等
+ * 给你长度相等的两个字符串 s1 和 s2 。一次 <b>字符串交换</b> 操作的步骤如下：选出某个字符串中的两个下标（不必不同），并交换这两个下标所对应的字符。
+ * <p>
+ * 如果对 <b>其中一个字符串</b> 执行 <b>最多一次字符串交换</b> 就可以使两个字符串相等，返回 true；否则，返回 false
+ * <p>
+ * <p>
+ * 示例 1：
+ * <pre>
+ * 输入：s1 = "bank", s2 = "kanb"
+ * 输出：true
+ * 解释：例如，交换 s2 中的第一个和最后一个字符可以得到 "bank"
+ * </pre>
+ * <p>
+ * 示例 2：
+ * <pre>
+ * 输入：s1 = "attack", s2 = "defend"
+ * 输出：false
+ * 解释：一次字符串交换无法使两个字符串相等
+ * </pre>
+ * <p>
+ * 示例 3：
+ * <pre>
+ * 输入：s1 = "kelb", s2 = "kelb"
+ * 输出：true
+ * 解释：两个字符串已经相等，所以不需要进行字符串交换
+ * </pre>
+ * <p>
+ * 示例 4：
+ * <pre>
+ * 输入：s1 = "abcd", s2 = "dcba"
+ * 输出：false
+ * </pre>
+ *
+ * @author yanggj
+ * @version 1.0.0
+ * @date 2022/10/11 10:11
+ */
+public class AreAlmostEqual {
+
+    /**
+     * <p>
+     * 题目要求其中一个字符串执行最多一次字符交换使得两个字符串相等,
+     * 意味着两个字符串中最多只存在两个位置 i,j 处字符不相等,
+     * 此时我们交换 i,j 处字符可使其相等。
+     * </p>
+     * <p>
+     * 设两个字符串分别为 s1,s2
+     * <lu>
+     * <li>
+     * 如果两个字符串相等，则不需要进行交换即可满足
+     * </li>
+     * <li>
+     * 如果两个字符串不等，则一定存在两个位置 i，j 的字符不相等，需要交换两处字符使其相等，
+     * 时必定满足s1[i]==s2[j],s1[j]==s2[i]，否则无法一次交换使其相等
+     * </li>
+     * </lu>
+     */
+    public boolean areAlmostEqual(String s1, String s2) {
+        int n = s1.length();
+        List<Integer> diff = new ArrayList<Integer>();
+        for (int i = 0; i < n; ++i) {
+            if (s1.charAt(i) == s2.charAt(i)) continue;
+            if (diff.size() >= 2) {
+                return false;
+            }
+            diff.add(i);
+        }
+        if (diff.isEmpty()) {
+            return true;
+        }
+        if (diff.size() != 2) {
+            return false;
+        }
+        return s1.charAt(diff.get(0)) == s2.charAt(diff.get(1)) && s1.charAt(diff.get(1)) == s2.charAt(diff.get(0));
+    }
+
+}
---
+++ src/main/java/com/guangjian/algorithm/daily/NumComponents.java
@@ -0,0 +1,68 @@
+package com.guangjian.algorithm.daily;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 链表组件
+ * 给定链表头结点 head，该链表上的每个结点都有一个 唯一的整型值 。同时给定列表 nums，该列表是上述链表中整型值的一个子集。
+ * <p>
+ * 返回列表 nums 中组件的个数，这里对组件的定义为：链表中一段最长连续结点的值（该值必须在列表 nums 中）构成的集合。
+ *
+ * @author yanggj
+ * @version 1.0.0
+ * @date 2022/10/12 16:29
+ */
+public class NumComponents {
+
+    /**
+     * 此题需要计算组件的个数，只需在链表中计算有多少组件的起始位置即可。当一个节点满足以下条件之一时，它是组件的起始位置：
+     * <p>
+     * <lu>
+     * <li>节点的值在数组 nums 中且节点位于链表起始位置；</li>
+     * <li>节点的值在数组 nums 中且节点的前一个点不在数组 nums 中。</li>
+     * </lu>
+     * <p>
+     * 遍历链表，计算出满足条件的点的个数即可。因为需要多次判断值是否位于数组 nums 中，用一个哈希集合保存数组 nums 中的点可以降低时间复杂度。
+     */
+    public int numComponents(ListNode head, int[] nums) {
+        // 用 set 存放 nums
+        Set<Integer> numsSet = new HashSet<>();
+        for (int num : nums) {
+            numsSet.add(num);
+        }
+        // 标记符
+        boolean inSet = false;
+        // 记录组件的个数
+        int res = 0;
+        while (head != null) {
+            if (numsSet.contains(head.val)) {
+                if (!inSet) {
+                    inSet = true;
+                    res++;
+                }
+            } else {
+                inSet = false;
+            }
+            head = head.next;
+        }
+        return res;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
