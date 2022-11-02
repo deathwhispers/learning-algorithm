e55354a8069bd4d3ba1e228dd92bd74cb2866236
杨光剑 <yangguangjian@rhytech.com.cn>
2022-11-02 09:57:45 +0800

    删除数组中重复元素

--- src/main/java/com/guangjian/algorithm/doublepointer/RemoveDuplicates.java
+++ src/main/java/com/guangjian/algorithm/doublepointer/RemoveDuplicates.java
@@ -1,12 +1,21 @@
 package com.guangjian.algorithm.doublepointer;
 
 /**
+ * <b>删除数组中重复元素</b>
+ *
  * @author yanggj
  * @version 1.0.0
  * @date 2022/11/1 17:35
  */
 public class RemoveDuplicates {
 
+    /**
+     * 使用双指针法
+     * <p>
+     * 指针 i 指向原数组, j 指向新的数组
+     * <p>
+     * 当 i,j 所指元素不一样时,将 i 赋值给 j
+     */
     public int removeDuplicates(int[] nums) {
         int j = 0;
         for (int i = 0; i < nums.length; i++) {
